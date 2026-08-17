2b2ed6ed9431141a38ed32490878f1f2095af35e
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-17 20:51:21 +0800

    feat: Add Header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+const MAGIC: u32 = 0x5350494e;
+
+struct Header {
+    magic: u32,
+    flags: u16,
+    constant_pool_count: u32,
+    constant_pool_offset: u32,
+    functions_count: u32,
+    functions_offset: u32
+}
 fn main() {
     println!("Hello, spina!");
 }
