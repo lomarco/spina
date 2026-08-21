f9e84f8726ce314e12064e5e7580650a7a1fdcac
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-21 23:36:21 +0800

    refactor: Rename `function_offset` to `functions_offset`

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,8 @@ fn main() -> std::io::Result<()> {
     let mut functions_count = [0u8; 4];
     reader.read_exact(&mut functions_count)?;
 
-    let mut function_offset = [0u8; 4];
-    reader.read_exact(&mut function_offset)?;
+    let mut functions_offset = [0u8; 4];
+    reader.read_exact(&mut functions_offset)?;
 
     let mut hdr = Header {
         magic: u32::from_le_bytes(magic),
@@ -65,7 +65,7 @@ fn main() -> std::io::Result<()> {
         constant_pool_count: u32::from_le_bytes(constant_pool_count),
         constant_pool_offset: u32::from_le_bytes(constant_pool_offset),
         functions_count: u32::from_le_bytes(functions_count),
-        functions_offset: u32::from_le_bytes(function_offset),
+        functions_offset: u32::from_le_bytes(functions_offset),
     };
 
     Ok(())
