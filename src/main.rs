dacad9ebc005b651c7549c2470036eaccb18dd11
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-21 15:26:14 +0800

    feat: Add the rest fields of Header struct

--- src/main.rs
+++ src/main.rs
@@ -48,5 +48,29 @@ fn main() -> std::io::Result<()> {
         println!("magic fault: '{}'", std::str::from_utf8(&magic).expect("Magic number fault"));
     }
 
+    let mut flags = [0u8; 2];
+    reader.read_exact(&mut flags)?;
+
+    let mut constant_pool_count = [0u8; 4];
+    reader.read_exact(&mut constant_pool_count)?;
+
+    let mut constant_pool_offset = [0u8; 4];
+    reader.read_exact(&mut constant_pool_offset)?;
+
+    let mut functions_count = [0u8; 4];
+    reader.read_exact(&mut functions_count)?;
+
+    let mut function_offset = [0u8; 4];
+    reader.read_exact(&mut function_offset)?;
+
+    let mut hdr = Header {
+        magic: u32::from_le_bytes(magic),
+        flags: Flags::from(u16::from_le_bytes(flags)),
+        constant_pool_count: u32::from_le_bytes(constant_pool_count),
+        constant_pool_offset: u32::from_le_bytes(constant_pool_offset),
+        functions_count: u32::from_le_bytes(functions_count),
+        functions_offset: u32::from_le_bytes(function_offset),
+    };
+
     Ok(())
 }
