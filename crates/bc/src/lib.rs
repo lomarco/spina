c6433a8a1f74e83c9a59eaa9c81d479dd744e5fa
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-22 17:38:50 +0800

    refactor: Delete excess tests

--- crates/bc/src/lib.rs
+++ crates/bc/src/lib.rs
@@ -122,14 +122,3 @@ impl Spc {
         Ok(())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
