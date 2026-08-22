a0981d58b3bd67fb21145b180c1a11f55880dc65
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-22 13:00:08 +0800

    feat: Move structs to bc/src/lib.rs

--- crates/bc/src/lib.rs
+++ crates/bc/src/lib.rs
@@ -1,5 +1,58 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+const MAGIC: u32 = 0x5350494e;
+
+struct Flags(u16);
+
+impl Flags {
+    const OPTIMIZE: Self = Self(1 << 0);
+    const DEBUG: Self     = Self(1 << 1);
+
+    fn from(bits: u16) -> Self {
+        Self(bits)
+    }
+}
+
+struct Header {
+    magic: u32,
+    flags: Flags,
+    constant_pool_count: u32,
+    constant_pool_offset: u32,
+    functions_count: u32,
+    functions_offset: u32
+}
+
+enum Tags {
+    Int,
+    Float,
+    IntRef,
+    FloatRef,
+    FuncRef
+}
+
+enum Data {
+    Int(i32),
+    Float(f32),
+    IntRef(u32),
+    FloatRef(u32),
+    FuncRef(u32)
+}
+
+struct Constant {
+    tag: Tags,
+    data_len: u32,
+    data: Data
+}
+
+struct Function {
+    name_index: u32,
+    param_count: u16,
+    locals_count: u16,
+    code: Vec<u8>
+}
+
+struct Spc {
+    header: Header,
+    constant: Constant,
+    functions: Function,
 }
 
 #[cfg(test)]
--- crates/core/main.rs
+++ crates/core/main.rs
@@ -7,56 +7,9 @@ use std::{
     env // FIXME: Replace this code to clap
 };
 
-const MAGIC: u32 = 0x5350494e;
-
-struct Flags(u16);
-
-impl Flags {
-    const OPTIMIZE: Self = Self(1 << 0);
-    const DEBUG: Self     = Self(1 << 1);
-
-    fn from(bits: u16) -> Self {
-        Self(bits)
-    }
-}
-
-struct Header {
-    magic: u32,
-    flags: Flags,
-    constant_pool_count: u32,
-    constant_pool_offset: u32,
-    functions_count: u32,
-    functions_offset: u32
-}
-
-enum Tags {
-    Int,
-    Float,
-    IntRef,
-    FloatRef,
-    FuncRef
-}
-
-enum Data {
-    Int(i32),
-    Float(f32),
-    IntRef(u32),
-    FloatRef(u32),
-    FuncRef(u32)
-}
-
-struct Constant {
-    tag: Tags,
-    data_len: u32,
-    data: Data
-}
-
-struct Function {
-    name_index: u32,
-    param_count: u16,
-    locals_count: u16,
-    code: Vec<u8>
-}
+use bc::{
+    ;
+};
 
 fn main() -> std::io::Result<()> {
     // FIXME: Replace this code to clap
