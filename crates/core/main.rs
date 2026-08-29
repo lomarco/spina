5a78280914be3c4128e8ae50b47195f00038fceb
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-29 21:58:42 +0800

    feat: Add run() func

--- crates/core/main.rs
+++ crates/core/main.rs
@@ -2,8 +2,9 @@ use std::env; // FIXME: Replace this code to clap
 use lex::flex;
 use std::fs::File;
 use std::io::Read;
+use std::process::ExitCode;
 
-fn main() -> std::io::Result<()> {
+fn run() -> Result<()> {
     // FIXME: Replace this code to clap
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -16,8 +17,18 @@ fn main() -> std::io::Result<()> {
     File::open(filename)?.read_to_string(&mut cont)?;
     match flex(&cont) {
         Ok(tokens) => println!("{tokens:?}"),
-        Err(err) => println!("{err}")
+        Err(err) => return Err(err.into()),
     }
 
     Ok(())
+}
+
+fn main() -> ExitCode {
+    match run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(e) => {
+            eprintln!("Error: {e:?}");
+            ExitCode::FAILURE
+        }
+    }
 }
