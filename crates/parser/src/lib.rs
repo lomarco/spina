6b113f6913f887821e0afcade69e703dbdfe8e79
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-31 15:33:49 +0800

    feat: Add parser

---
+++ crates/parser/src/lib.rs
@@ -0,0 +1,84 @@
+pub enum BinOpKind {
+    /// The `+` operator (addition)
+    Add,
+    /// The `-` operator (subtraction)
+    Sub,
+    /// The `*` operator (multiplication)
+    Mul,
+    /// The `/` operator (division)
+    Div,
+    /// The `%` operator (modulus)
+    Rem,
+    /// The `&&` operator (logical and)
+    And,
+    /// The `||` operator (logical or)
+    Or,
+    /// The `^` operator (bitwise xor)
+    BitXor,
+    /// The `&` operator (bitwise and)
+    BitAnd,
+    /// The `|` operator (bitwise or)
+    BitOr,
+    /// The `<<` operator (shift left)
+    Shl,
+    /// The `>>` operator (shift right)
+    Shr,
+    /// The `==` operator (equality)
+    Eq,
+    /// The `<` operator (less than)
+    Lt,
+    /// The `<=` operator (less than or equal to)
+    Le,
+    /// The `!=` operator (not equal to)
+    Ne,
+    /// The `>=` operator (greater than or equal to)
+    Ge,
+    /// The `>` operator (greater than)
+    Gt,
+}
+
+pub struct Spanned<T> {
+    pub node: T,
+    pub span: Span,
+}
+
+pub type BinOp = Spanned<BinOpKind>;
+
+pub enum UnOp {
+    /// The `*` operator for dereferencing
+    Deref,
+    /// The `!` operator for logical inversion
+    Not,
+    /// The `-` operator for negation
+    Neg,
+}
+
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+    pub tokens: 
+}
+
+pub enum ExprKind {
+    Array(Vec<Box<Expr>>),
+    Call(Box<Expr>, Vec<Box<Expr>>),
+    Tup(Vec<Box<Expr>>),
+    Binary(BinOp, Box<Expr>, Box<Expr>),
+    Unary(UnOp, Box<Expr>),
+    Lit(token::Lit),
+    Let(Box<Pat>, Box<Expr>, Span),
+    If(Box<Expr>, Box<Block>, Option<Box<Expr>>),
+    While(Box<Expr>, Box<Block>, Option<Label>),
+    ForLoop(Box<ForLoop>),
+    Loop(Box<Block>, Option<Label>, Span),
+    Block(Box<Block>, Option<Label>),
+    Field(Box<Expr>, Ident),
+    Index(Box<Expr>, Box<Expr>, Span),
+    Break(Option<Label>, Option<Box<Expr>>),
+    Continue(Option<Label>),
+    Ret(Option<Box<Expr>>),
+}
+
+pub struct Parse {
+    tokens: Vec<SpannedToken<'source>>,
+}
